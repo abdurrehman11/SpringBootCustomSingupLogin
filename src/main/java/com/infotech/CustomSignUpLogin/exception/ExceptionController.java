7fc542feed693ebd8754eed1c7153adb1bc452b7
Abdur Rehman <rizwan.majeed@venturedive.com>
2018-10-12 15:29:28 +0500

    Update Response Status

--- src/main/java/com/infotech/CustomSignUpLogin/exception/ExceptionController.java
+++ src/main/java/com/infotech/CustomSignUpLogin/exception/ExceptionController.java
@@ -3,15 +3,19 @@ package com.infotech.CustomSignUpLogin.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @ControllerAdvice
 public class ExceptionController {
 
     @ResponseBody
     @ExceptionHandler(value = BaseException.class)
-    public Response handleValidationException (BaseException ex) {
+    public Response handleValidationException (BaseException ex, HttpServletRequest req, HttpServletResponse res) {
 
        Response response = new Response(ex.getHttpStatus().value(), ex.getErrorMessage(),
                ex.getErrorCode());
+       res.setStatus(ex.getHttpStatus().value());
        return response;
     }
 
